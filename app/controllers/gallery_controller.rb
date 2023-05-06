8c31c0224bc3a1c3e32ad4d8e31e6dcd63dba29a
Alex Lazarev <mortaleusmarketing@gmail.com>
2023-05-06 08:05:48 -0400

    fixed bucket issue

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -2,13 +2,13 @@ class GalleryController < ApplicationController
     def index
         @gallery_imgs = GalleryImg.all.with_attached_image
     end
-
+    
     def create
         gallery_img = GalleryImg.create!
         gallery_img.image.attach(params[:image])
         gallery_img.save
         redirect_to gallery_path
-    end
+      end
 
     def destroy
         gallery_img = GalleryImg.find(params[:id])
